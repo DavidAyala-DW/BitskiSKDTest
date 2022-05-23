d6aa9364fd93a2fe719996aed61e2be97b41ebe8
LuisZanabria <luis.zanabria2@unmsm.edu.pe>
2022-05-23 16:19:57 -0500

    add scroll to featured product when explore button is pressed

--- components/Main.jsx
+++ components/Main.jsx
@@ -17,7 +17,7 @@ function Main({featuredProduct}) {
         </h2>
       </div>
 
-      <div className="flex flex-col space-y-10 lg:space-y-0 lg:space-x-5 xl:space-x-0 lg:flex-row md:items-center justify-between w-full max-w-[1140px] base:max-w-[calc(89.0625%)] mr-auto">
+      <div className="flex flex-col space-y-10 lg:space-y-0 lg:space-x-5 xl:space-x-0 lg:flex-row md:items-center justify-between w-full max-w-[1140px] base:max-w-[calc(89.0625%)] mr-auto" id="featuredProduct">
 
         <div className="flex flex-col items-start">
           <h2 className='text-[32px] leading-[32px] md:text-[52px] font-headline md:font-black md:max-w-[552px] md:mx-auto md:text-center lg:text-left lg:mx-0 lg:max-w-[600px] md:leading-[52px] mb-4 md:mb-6'>
